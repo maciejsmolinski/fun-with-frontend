ac6bff498cdff1c06defcb77de10f0601aec3bbd
Maciej Smolinski <contact@maciejsmolinski.com>
2019-05-04 23:14:00 +0200

    Examples: Render text in the html for each route.

--- src/App.purs
+++ src/App.purs
@@ -1,11 +1,21 @@
 module App where
 
 import Prelude
+import Prelude
 
+import Data.Maybe (fromJust, isJust)
 import Effect (Effect)
 import History as History
 import Location as Location
 import Logger as Logger
+import Partial.Unsafe (unsafePartial)
+import Web.DOM.Document as Document
+import Web.DOM.Element as Element
+import Web.DOM.Node (appendChild, setTextContent)
+import Web.HTML as HTML
+import Web.HTML.HTMLDocument as HTMLDocument
+import Web.HTML.HTMLElement as HTMLElement
+import Web.HTML.Window as Window
 
 data Route = Home | NotFound
 
@@ -16,8 +26,8 @@ toRoute _ = NotFound
 render :: Route -> Effect Unit
 render =
   case _ of
-    Home -> Logger.log "Home Route"
-    NotFound -> Logger.log "Route Not Found"
+    Home -> renderText "Home Route" *> Logger.log "Home Route"
+    NotFound -> renderText "Route Not Found" *> Logger.log "Route Not Found"
 
 type App = { onRoute :: String -> Effect Unit }
 
@@ -30,6 +40,15 @@ main :: Effect Unit
 main = createApp { onRoute }
  where
    onRoute = case _ of
-      "/redirect" -> History.setPathName "/"
+      "/redirect" -> History.setPathName "/" *> main
       other -> render $ toRoute other
 
+renderText :: String -> Effect Unit
+renderText text = unsafePartial do
+  documentEl <- HTML.window >>= Window.document
+  maybeBodyEl <- HTMLDocument.body documentEl
+  when (isJust maybeBodyEl) do
+    let bodyEl = fromJust maybeBodyEl
+    divEl <- Document.createElement "div" (HTMLDocument.toDocument documentEl)
+    setTextContent text (Element.toNode divEl)
+    void $ appendChild (Element.toNode divEl) (HTMLElement.toNode bodyEl)
